1ee5499c60a90a183f17b41a2743c24eb5497b45
HIBBANUR RAHMAN <117516111+Hibbanur-Rahman@users.noreply.github.com>
2023-04-06 02:13:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,74 +8,88 @@ var x = document.getElementsByTagName("button");
 console.log(x)
 var y = document.getElementsByClassName("demo");
 console.log(y);
+
 var i = 1;
-var flag=-1;
+var flag = -1;
 function myfunc(val) {
-    if(flag==1 || flag==2)
-    {
+    if (flag == 1 || flag == 2 && flag!=3) {
         alert("you cannot play further more because the game is end");
     }
     else{
-    if (i % 2 == 0) {
-        player2.push(val);
-        y[val - 1].innerHTML = "O";
-        y[val-1].style.fontSize="50px";
-        console.log(player2);
-        console.log(win_dec(player2));
-        if(win_dec(player2)==true)
-        {
-            
-            for(var b=0;b<3;b++)
-            {
-                x[a[b]-1].style.backgroundColor="green";
+        if (i % 2 == 0) {
+            player2.push(val);
+            y[val - 1].innerHTML = "O";
+            y[val - 1].style.fontSize = "50px";
+            console.log(player2);
+            console.log(win_dec(player2));
+            if (win_dec(player2) == true) {
+
+                for (var b = 0; b < 3; b++) {
+                    x[a[b] - 1].style.backgroundColor = "green";
+                }
+                alert("player2 win");
+                flag = 2;
             }
-            alert("player2 win");
-            flag=2;
+            i++;
         }
-        i++;
-    }
-    else {
-        player1.push(val);
-        y[val - 1].innerHTML = "X";
-        y[val-1].style.fontSize="50px";
-        win_dec(player1);
-        i++;
-        console.log(player1);
-        console.log(win_dec(player1));
-        if(win_dec(player1)==true)
-        {
-            
-            for(var b=0;b<3;b++)
-            {
-                x[a[b]-1].style.backgroundColor="red";
+        else {
+            player1.push(val);
+            y[val - 1].innerHTML = "X";
+            y[val - 1].style.fontSize = "50px";
+            win_dec(player1);
+            i++;
+            console.log(player1);
+            console.log(win_dec(player1));
+            if (win_dec(player1) == true) {
+
+                for (var b = 0; b < 3; b++) {
+                    x[a[b] - 1].style.backgroundColor = "red";
+                }
+                alert("player1 win");
+                flag = 1;
             }
-            alert("player1 win");
-            flag=1;
         }
     }
 }
-}
-var a=[];
+var a = [];
 function win_dec(z) {
     var count = 0;
 
     for (var k = 0; k < win.length; k++) {
         for (var l = 0; l < 3; l++) {
             for (var j = 0; j < z.length; j++) {
-                const c=z.filter(value => value == z[j]);
+                const c = z.filter(value => value == z[j]);
                 console.log(c);
-                if (z[j] == win[k][l] && c.length==1) {
+                if (z[j] == win[k][l] && c.length == 1) {
                     count++;
                 }
                 if (count == 3) {
-                    a=win[k];
+                    a = win[k];
                     console.log(a)
                     return true;
                 }
             }
         }
-       
+
         count = 0;
     }
     return false;
+}
+
+
+const playagain = document.getElementById('play');
+function play() {
+    for (var b = 0; b < player1.length; b++) {
+        y[player1[b] - 1].innerHTML = "";
+    }
+    for (var b = 0; b < player2.length; b++) {
+        y[player2[b] - 1].innerHTML = "";
+    }
+    for (var b = 0; b < 3; b++) {
+        x[a[b] - 1].style.backgroundColor = "rgb(209, 227, 227)";
+    }
+    player1 = [];
+    player2 = [];
+    flag=3;
+
 }
